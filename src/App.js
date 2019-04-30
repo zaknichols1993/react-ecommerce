596272bd5c0d9544d8c5ce3fae34ab06aa15eed1
Zak Nichols <zaknichols1993@gmail.com>
2019-04-30 18:44:26 -0400

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 text-center">1</div>
-        <div className="col-sm-6 text-center">2</div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand"></img>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
